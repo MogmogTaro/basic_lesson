d3b597aa7db46ecd5682d9d61baabcf2fedbf9c8
UCDM <palama.ucdm.1212@gmail.com>
2022-01-11 16:47:57 +0900

    useEffectのcleanUp

---
+++ src/CleanUp.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import { increment } from './features/counter/counterSlice';
+
+export const CleanUp = () => {
+  const [currentNum, setCurrentNum] = useState(0);
+
+  const incrementNum = () => {
+    console.log("mouseEvent invoked!")
+    setCurrentNum((preNumber) => preNumber + 1)
+  }
+
+  useEffect(() => {
+    console.log("useEffect in CleanUp invoked!")
+    window.addEventListener("mousedown", incrementNum);
+    return () => {
+      console.log("Cleanup invoked!");
+      window.removeEventListener("mousedown", incrementNum);
+    }
+  })
+  return (
+    <div>
+      {currentNum}
+    </div>
+  )
+}
